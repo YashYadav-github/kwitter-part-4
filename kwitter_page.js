4be970e8651d15cbd53f38b9cb0d28626630314d
YashYadav-github <74818709+YashYadav-github@users.noreply.github.com>
2022-06-25 13:46:44 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,13 @@
+
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+
+function send()
+{
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+   });
+}
